922623e71831eabe9239733fa4e284a187fd605d
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-05 23:33:27 +0530

    Version : 1.1.1 : Added NavBar.

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 Website Name - Postman-Clone
 Name : Vanshul Kesharwani
 Date : 05/04/2022
-Version : 1.0.0
+Version : 1.1.1
 Email : vkvanshulkesharwani54@gmail.com
 Description : This is a Web app which can give us post and get request reply. Working on JavaScript. This project works without refreshing page.
 */
