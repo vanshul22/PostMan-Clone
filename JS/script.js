e32c50884b59b46aab0c5ba80b5143d4d65fc37e
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-06 01:07:13 +0530

    Version 2.3.1 : Addedparametersbox,jsonBox, button and responsebox in UI.

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 Website Name - Postman-Clone
 Name : Vanshul Kesharwani
 Date : 05/04/2022
-Version : 2.2.1
+Version : 2.3.1
 Email : vkvanshulkesharwani54@gmail.com
 Description : This is a Web app which can give us post and get request reply. Working on JavaScript. This project works without refreshing page.
 */
