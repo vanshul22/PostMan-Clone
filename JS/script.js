fd7098c6dd6ef062ba81984f55893bced2fb0af2
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-05 22:59:00 +0530

    Version 1.0.0 : Initial Commit.

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+/*
+Website Name - Postman-Clone
+Name : Vanshul Kesharwani
+Date : 05/04/2022
+Version : 1.0.0
+Email : vkvanshulkesharwani54@gmail.com
+Description : This is a Web app which can give us post and get request reply. Working on JavaScript. This project works without refreshing page.
+*/
