178e407012ae274f350e8179a59a03502c22081c
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-06 15:20:08 +0530

    Version : 4.2.1 : Added functionality of Submit button for JSON & custom type data.

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 Website Name - Postman-Clone
 Name : Vanshul Kesharwani
 Date : 05/04/2022
-Version : 3.1.1
+Version : 4.2.1
 Email : vkvanshulkesharwani54@gmail.com
 Description : This is a Web app which can give us post and get request reply. Working on JavaScript. This project works without refreshing page.
 */
@@ -44,12 +44,12 @@ addParametersBtn.addEventListener("click", () => {
     let newParameters = document.getElementById("newParameters");
     let stringOfParameters = `  
         <div class="row mb-3">
-            <label for="parameterKey${parametersCount+2}" class="col-sm-2 col-form-label">Parameter ${parametersCount+2} :</label>
+            <label for="parameterKey${parametersCount + 2}" class="col-sm-2 col-form-label">Parameter ${parametersCount + 2} :</label>
             <div class="col-sm-4">
-                <input type="text" class="form-control" id="parameterKey${parametersCount+2}" placeholder="Enter parameter ${parametersCount+2} Key">
+                <input type="text" class="form-control" id="parameterKey${parametersCount + 2}" placeholder="Enter parameter ${parametersCount + 2} Key">
             </div>
             <div class="col-sm-5">
-                <input type="text" class="form-control" id="parameterValue${parametersCount+2}" placeholder="Enter parameter ${parametersCount+2} Value">
+                <input type="text" class="form-control" id="parameterValue${parametersCount + 2}" placeholder="Enter parameter ${parametersCount + 2} Value">
             </div>
             <button class="col-sm-1 btn btn-dark deleteParameters"> - </button>
         </div>`;
@@ -65,5 +65,37 @@ addParametersBtn.addEventListener("click", () => {
             // removing selected parameters from here.
             e.target.parentElement.remove();
         });
+    };
+});
+
+// If user click on submit button.
+let submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", () => {
+    // Show "Please Wait" in the response box.
+    document.getElementById("responseText").value = "Please wait fetching response...";
+    // Fetch all the values that user entered.
+    let url = document.getElementById("url").value;
+    // Select element by their name. Whatever the element is checked it will give the value of element.
+    let requestType = document.querySelector("input[name='requestType']:checked").value;
+    let contentType = document.querySelector("input[name='contentType']:checked").value;
+
+    // if user select parameters instead of JSON, Collect all parameters in object.
+    if (contentType == "customParameter") {
+        // Assign empty dataObj.
+        let dataObj = {};
+        for (let i = 0; i < parametersCount + 1; i++) {
+            // Checking for deleted parameters.
+            if (document.getElementById("parameterValue" + (i + 1)) != undefined) {
+                // taking values by parameters.
+                let keyOfParameter = document.getElementById("parameterKey" + (i + 1)).value;
+                let valueOfParameter = document.getElementById("parameterValue" + (i + 1)).value;
+                // Adding key value pair in our object dataObj.
+                dataObj[keyOfParameter] = valueOfParameter;
+            };
+            // Convert object into string here.
+            dataObj = JSON.stringify(dataObj);
+        };
+    } else {
+        dataObj = document.getElementById("jsonText").value;
     };
 });
